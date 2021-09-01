d208015dbfbf11f4efdeafe62154066bf0ffe496
Soule BA <soule@weave.works>
2021-09-01 13:39:44 +0200

    Add a new client relying on github.com/hashicorp/go-retryablehttp.
    custom retry and backoff strategy have been added, as well as a rate
    limiter.
    
    It is heavily based on the existing gitlab provider client (github.com/xanzy/go-gitlab)
    
    Signed-off-by: Soule BA <soule@weave.works>

--- go.mod
+++ go.mod
@@ -4,13 +4,21 @@ go 1.15
 
 require (
 	github.com/go-logr/logr v1.1.0
+	github.com/go-logr/zapr v1.1.0
 	github.com/google/go-cmp v0.4.0
 	github.com/google/go-github/v32 v32.1.0
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
+	github.com/hashicorp/go-cleanhttp v0.5.1
+	github.com/hashicorp/go-retryablehttp v0.6.4
+	github.com/kr/text v0.2.0 // indirect
 	github.com/ktrysmt/go-bitbucket v0.6.2
 	github.com/onsi/ginkgo v1.14.0
 	github.com/onsi/gomega v1.10.1
 	github.com/xanzy/go-gitlab v0.43.0
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
-	golang.org/x/oauth2 v0.0.0-20181106182150-f42d05182288
+	go.uber.org/zap v1.19.0
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
+	golang.org/x/tools v0.1.5 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
---
+++ http/client_test.go
@@ -0,0 +1,341 @@
+package http
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
+	"go.uber.org/zap/zaptest"
+)
+
+func Test_NewRequester(t *testing.T) {
+	tests := []struct {
+		name      string
+		host      string
+		header    *http.Header
+		timeout   time.Duration
+		client    *http.Client
+		transport http.RoundTripper
+		log       *logr.Logger
+		output    string
+	}{
+		{
+			name:      "no host",
+			host:      "",
+			header:    &http.Header{},
+			timeout:   defaultTimeout,
+			client:    &http.Client{},
+			transport: defaultTransport,
+			log:       &logr.Logger{},
+			output:    "host is required",
+		},
+		{
+			name:      "wrong host",
+			host:      "local#.@*dev",
+			header:    &http.Header{},
+			timeout:   defaultTimeout,
+			client:    &http.Client{},
+			transport: defaultTransport,
+			log:       &logr.Logger{},
+			output:    `failed to parse host https://local#.@*dev to url, parse "https://local#.@*dev": net/url: invalid userinfo`,
+		},
+		{
+			name:   "default host",
+			log:    &logr.Logger{},
+			host:   fmt.Sprint("http://" + defaultHost),
+			output: fmt.Sprint("http://" + defaultHost),
+		}, {
+			name:      "my specific host",
+			host:      "my.host",
+			header:    &http.Header{},
+			timeout:   defaultTimeout,
+			client:    &http.Client{Transport: defaultTransport},
+			transport: defaultTransport,
+			log:       initLogger(t),
+			output:    "https://my.host",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var val string
+			c, err := NewClient(&http.Client{}, tt.host, tt.header, tt.log, func(c *Client) {
+				c.Client.HTTPClient = &http.Client{
+					Transport: tt.transport,
+					Timeout:   tt.timeout,
+				}
+			})
+			if err != nil {
+				val = fmt.Sprintf("%s", err)
+			}
+			if c != nil {
+				val = c.BaseURL.String()
+			}
+			if val != tt.output {
+				t.Errorf("Expected %s, got %s", tt.output, val)
+			}
+		})
+	}
+}
+
+func Test_Do(t *testing.T) {
+	type user struct {
+		Name  []string `json:"name"`
+		Email []string `json:"email"`
+	}
+
+	tests := []struct {
+		name   string
+		path   string
+		query  url.Values
+		method string
+		body   interface{}
+		header http.Header
+		output interface{}
+	}{
+		{
+			name: "test GET method",
+			path: "users",
+			query: url.Values{
+				"name":  []string{"john", "doe"},
+				"email": []string{"jdoe@programmer.net"},
+			},
+			method: http.MethodGet,
+			output: user{
+				Name:  []string{"john", "doe"},
+				Email: []string{"jdoe@programmer.net"},
+			},
+		}, {
+			name:   "test POST method",
+			path:   "users",
+			method: http.MethodPost,
+			header: http.Header{
+				"Content-Type": []string{"application/json"},
+			},
+			body: &user{
+				Name:  []string{"tony", "stark"},
+				Email: []string{"tony@stark.entreprise"},
+			},
+			output: []byte("201 - OK!"),
+		},
+		{
+			name:   "test POST with wrong headers method",
+			path:   "users",
+			method: http.MethodPost,
+			header: http.Header{
+				"Content-Type": []string{"text/html"},
+			},
+			body: &user{
+				Name:  []string{"tony", "stark"},
+				Email: []string{"tony@stark.entreprise"},
+			},
+			output: []byte("400 - NOK!"),
+		},
+		{
+			name:   "test POST without body",
+			path:   "users",
+			method: http.MethodPost,
+			header: http.Header{
+				"Content-Type": []string{"application/json"},
+			},
+			output: []byte("201 - OK!"),
+		},
+	}
+
+	berearHeader := &http.Header{
+		"WWW-Authenticate": []string{"Bearer"},
+	}
+
+	// declare a Client
+	c, err := NewClient(nil, defaultHost, berearHeader, initLogger(t))
+	if err != nil {
+		t.Errorf("unexpected error while declaring a client: %v", err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Start a local HTTP server
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				var resp []byte
+				if ctype := req.Header.Get("WWW-Authenticate"); ctype != "Bearer" {
+					w.WriteHeader(http.StatusBadRequest)
+					w.Write([]byte("400 - NOK!"))
+					return
+				}
+				switch req.Method {
+				case http.MethodGet:
+					response, err := json.Marshal(tt.query)
+					if err != nil {
+						w.WriteHeader(http.StatusInternalServerError)
+						resp = []byte("500 - Failed to retrieve data!")
+					} else {
+						w.WriteHeader(http.StatusOK)
+						resp = response
+					}
+				case http.MethodPost:
+					if ctype := req.Header.Get("Content-Type"); ctype != "application/json" {
+						w.WriteHeader(http.StatusBadRequest)
+						resp = []byte("400 - NOK!")
+					} else {
+						w.WriteHeader(http.StatusCreated)
+						resp = []byte("201 - OK!")
+					}
+				case http.MethodPut:
+					w.WriteHeader(http.StatusCreated)
+					resp = []byte("201 - OK!")
+				}
+				w.Write(resp)
+			}))
+
+			// Close the server when test finishes
+			defer server.Close()
+
+			url, _ := url.ParseRequestURI(server.URL)
+			// tie Requester url and client to the fake server
+			c.BaseURL = url
+			c.Client.HTTPClient = server.Client()
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			req, err := c.NewRequest(ctx, tt.method, tt.path, tt.query, tt.body, tt.header)
+			if err != nil {
+				t.Fatalf("request generation failed with error: %v", err)
+			}
+
+			res, resp, err := c.Do(req)
+			if err != nil {
+				t.Fatalf("request failed with error: %v", err)
+			}
+			defer resp.Body.Close()
+
+			if tt.method == http.MethodGet {
+				user := user{}
+				// Restore the io.ReadCloser to its original state
+				resp.Body = ioutil.NopCloser(bytes.NewBuffer(res))
+				err := json.NewDecoder(resp.Body).Decode(&user)
+				if err != nil {
+					t.Fatalf("%s users failed, unable to obtain response body: %v", tt.method, err)
+				}
+				if !reflect.DeepEqual(user, tt.output) {
+					t.Errorf("Expected %s, got %s", tt.output, user)
+				}
+			} else {
+				if err != nil {
+					t.Fatalf("%s users failed, unable to obtain response body: %v", tt.method, err)
+				}
+				if !reflect.DeepEqual(res, tt.output) {
+					t.Errorf("Expected %s, got %s", tt.output, res)
+				}
+			}
+		})
+	}
+}
+
+func Test_DoWithRetry(t *testing.T) {
+	tests := []struct {
+		name               string
+		retries            int
+		retryMin, retryMax time.Duration
+		output             []byte
+	}{
+		{
+			name:     "test 2 retries",
+			retries:  2,
+			retryMin: 1 * time.Millisecond,
+			retryMax: 2 * time.Millisecond,
+			output:   []byte("2"),
+		},
+		{
+			name:     "test 5 retries",
+			retries:  5,
+			retryMin: 1 * time.Millisecond,
+			retryMax: 2 * time.Millisecond,
+			output:   []byte("5"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Start a local HTTP server
+			retries := 1
+			c := NewTestClient(t, func(req *http.Request) (*http.Response, error) {
+				if retries < tt.retries {
+					retries++
+					return nil, fmt.Errorf("connection refused, please retry.")
+				}
+				return &http.Response{
+					StatusCode: 200,
+					Body:       ioutil.NopCloser(bytes.NewBufferString(fmt.Sprint(retries))),
+					Header:     make(http.Header),
+				}, nil
+			}, func(c *Client) {
+				c.Client.RetryWaitMin = tt.retryMin
+				c.Client.RetryWaitMax = tt.retryMax
+				c.Client.RetryMax = tt.retries
+			})
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			request, err := c.NewRequest(ctx, http.MethodGet, "", nil, nil, nil)
+			if err != nil {
+				t.Fatalf("failed to create request: %v", err)
+			}
+
+			res, resp, err := c.Do(request)
+			defer resp.Body.Close()
+			if err != nil {
+				if !strings.Contains(err.Error(), string(tt.output)) {
+					t.Fatalf("request failed: %v", err)
+				}
+				return
+			}
+
+			if !reflect.DeepEqual(res, tt.output) {
+				t.Errorf("Expected %s, got %s", tt.output, res)
+			}
+		})
+	}
+}
+
+func initLogger(t *testing.T) *logr.Logger {
+	var log logr.Logger
+	zapLog := zaptest.NewLogger(t)
+	log = zapr.NewLogger(zapLog)
+	return &log
+}
+
+// RoundTripFunc .
+type RoundTripFunc func(req *http.Request) (*http.Response, error)
+
+// RoundTrip .
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req)
+}
+
+//NewTestClient returns a Client with Transport replaced to avoid making real calls
+func NewTestClient(t *testing.T, fn RoundTripFunc, opts ...ClientOptionsFunc) *Client {
+	c, err := NewClient(nil, defaultHost, nil, initLogger(t))
+	if err != nil {
+		t.Fatalf("unexpected error while declaring a client: %v", err)
+	}
+
+	c.Client.HTTPClient.Transport = RoundTripFunc(fn)
+
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	return c
+}

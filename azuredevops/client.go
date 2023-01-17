14b170547cf046121e4f49d92086276acbe92833
nellyk <nellykiboi@gmail.com>
2023-01-17 11:04:53 +0000

    add intial setup & get list of projects api

--- azuredevops/client.go
+++ azuredevops/client.go
@@ -1,1 +1,85 @@
+/*
+Copyright 2020 The Flux CD contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package azuredevops
+
+import (
+	"context"
+	"github.com/fluxcd/go-git-providers/gitprovider"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/core"
+)
+
+// ProviderID is the provider ID for AzureDevops.
+const ProviderID = gitprovider.ProviderID("azureDevops")
+
+func newClient(c core.Client, domain string, destructiveActions bool) *Client {
+	azureDevopsClient := &azureDevopsClientImpl{c, destructiveActions}
+	ctx := &clientContext{azureDevopsClient, domain, destructiveActions}
+	return &Client{
+		clientContext: ctx,
+		orgs: &OrganizationsClient{
+			clientContext: ctx,
+		},
+	}
+}
+
+// Client implements the gitprovider.Client interface.
+var _ gitprovider.Client = &Client{}
+
+type clientContext struct {
+	c                  azureDevopsClient
+	domain             string
+	destructiveActions bool
+}
+
+// Client is an interface that allows talking to a Git provider.
+type Client struct {
+	*clientContext
+	orgs *OrganizationsClient
+}
+
+func (c *Client) Organizations() gitprovider.OrganizationsClient {
+	return c.orgs
+}
+
+func (c Client) OrgRepositories() gitprovider.OrgRepositoriesClient {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c Client) UserRepositories() gitprovider.UserRepositoriesClient {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c Client) SupportedDomain() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Client) ProviderID() gitprovider.ProviderID {
+	return ProviderID
+}
+
+func (c Client) HasTokenPermission(ctx context.Context, permission gitprovider.TokenPermission) (bool, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c Client) Raw() interface{} {
+	//TODO implement me
+	panic("implement me")
+}
--- azuredevops/resource_organization.go
+++ azuredevops/resource_organization.go
@@ -1,1 +1,67 @@
+/*
+Copyright 2020 The Flux CD contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package azuredevops
+
+import (
+	"github.com/fluxcd/go-git-providers/gitprovider"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/core"
+)
+
+func newOrganization(ctx *clientContext, apiObj core.TeamProjectReference, ref gitprovider.OrganizationRef) *organization {
+	return &organization{
+		clientContext: ctx,
+		p:             apiObj,
+		ref:           ref,
+		teams: &TeamsClient{
+			clientContext: ctx,
+			ref:           ref,
+		},
+	}
+}
+
+var _ gitprovider.Organization = &organization{}
+
+type organization struct {
+	*clientContext
+
+	p   core.TeamProjectReference
+	ref gitprovider.OrganizationRef
+
+	teams *TeamsClient
+}
+
+func (o *organization) Teams() gitprovider.TeamsClient {
+	return o.teams
+}
+func (o *organization) Get() gitprovider.OrganizationInfo {
+	return organizationFromAPI(&o.p)
+}
+
+func (o *organization) APIObject() interface{} {
+	return &o.p
+}
+
+func (o *organization) Organization() gitprovider.OrganizationRef {
+	return o.ref
+}
+
+func organizationFromAPI(apiObj *core.TeamProjectReference) gitprovider.OrganizationInfo {
+	return gitprovider.OrganizationInfo{
+		Name:        (*apiObj).Name,
+		Description: (*apiObj).Description,
+	}
+}

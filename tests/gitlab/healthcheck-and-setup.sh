260eed495f1bb3b73008d99f73c524cb2219cb0a
Timo Furrer <tuxtimo@gmail.com>
2023-04-22 14:17:49 +0200

    Use services to bootstrap GitLab groups and projects
    Signed-off-by: Timo Furrer <tuxtimo@gmail.com>

--- tests/gitlab/healthcheck-and-setup.sh
+++ tests/gitlab/healthcheck-and-setup.sh
@@ -25,20 +25,11 @@ git_provider_user_pat = PersonalAccessToken.create(user_id: git_provider_user.id
 git_provider_user_pat.set_token('$GITLAB_TOKEN')
 git_provider_user_pat.save!
 
-git_provider_organization = Group.new(name: '$GIT_PROVIDER_ORGANIZATION', path: '$GIT_PROVIDER_ORGANIZATION')
-git_provider_organization.save!
-NamespaceSetting.new(namespace: git_provider_organization).save!
-git_provider_organization.add_owner(git_provider_user)
-
+# Group and Project setup
+git_provider_organization = ::Groups::CreateService.new(git_provider_user, { name: '$GIT_PROVIDER_ORGANIZATION', path: '$GIT_PROVIDER_ORGANIZATION' }).execute
+::Groups::CreateService.new(git_provider_user, { name: '$GITLAB_TEST_SUBGROUP', path: '$GITLAB_TEST_SUBGROUP', parent_id: git_provider_organization.id }).execute
 ::Projects::CreateService.new(git_provider_user, { name: '$GITLAB_TEST_REPO_NAME', namespace_id: git_provider_organization.id }).execute
-
-gitlab_test_subgroup = Group.new(name: '$GITLAB_TEST_SUBGROUP', path: '$GITLAB_TEST_SUBGROUP', parent: git_provider_organization)
-gitlab_test_subgroup.save!
-
-gitlab_test_team = Group.new(name: '$GITLAB_TEST_TEAM_NAME', path: '$GITLAB_TEST_TEAM_NAME')
-gitlab_test_team.save!
-NamespaceSetting.new(namespace: gitlab_test_team).save!
-gitlab_test_team.add_owner(git_provider_user)
+::Groups::CreateService.new(git_provider_user, { name: '$GITLAB_TEST_TEAM_NAME', path: '$GITLAB_TEST_TEAM_NAME' }).execute
 EOF
 
 	touch $done
